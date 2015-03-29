249e6ab1e23cb07549c985de5841f36cb1d0277e
Simon Charlow <simon.charlow@gmail.com>
2015-03-28 23:11:47 -0400

    Added some test cases

--- appos_mon.hs
+++ appos_mon.hs
@@ -27,7 +27,9 @@ instance Monad m => Monad (StateT m) where
                                   runStateT (k x) s'
 
 instance MonadTrans StateT where
-  lift m = StateT $ \s -> do { x <- m; return (x, s) }
+  lift m = StateT $ \s -> do
+                          x <- m
+                          return (x, s)
 
 instance Monad m => Functor (StateT m) where
   fmap f (StateT m) = StateT $ \s -> do
@@ -48,7 +50,9 @@ instance Monad m  => Monad (WriterT m) where
                               return (y, p && q)
 
 instance MonadTrans WriterT where
-  lift m = WriterT $ do { x <- m; return (x, True) }
+  lift m = WriterT $ do
+                     x <- m
+                     return (x, True)
 
 instance Monad m => Functor (WriterT m) where
   fmap f (WriterT m) = WriterT $ do
@@ -59,17 +63,68 @@ instance Monad m => Applicative (WriterT m) where
   pure  = return
   (<*>) = ap
 
-tick :: Monad m => StateT m E -> StateT m E
-tick m = do { x <- m; StateT $ \s -> runStateT (return x) (s++[x]) }
+--anaphora
+push :: Monad m => StateT m E -> StateT m E
+push m = do
+         x <- m
+         StateT $ \s -> runStateT (return x) (s++[x])
 
-inj :: Int -> WriterT (StateT List) Int
-inj x = lift . tick $ return x
+pro :: Monad m => Int -> StateT m E
+pro n = StateT $ \s -> return (reverse s !! n, s)
+
+--inj :: E -> WriterT (StateT List) E
+--inj x = lift . push $ return x
+
+-- NRCs
+type P  = StateT List Bool
+type EP = WriterT (StateT List) E
+
+comma :: (E -> P) -> E -> EP
+comma f x = WriterT $ do
+                      p <- f x
+                      return (x, p)
+
+--rcOdd :: E -> P
+--rcOdd x = return $ odd x
+
+--threeWhichIsOdd :: EP
+--threeWhichIsOdd = do
+--                  x <- inj 3
+--                  comma rcOdd x
+
+rcLTSucc :: E -> P
+rcLTSucc x = do
+             y <- pro 0
+             z <- push . return $ succ y
+             return $ (<) x z
+
+--threeWhichLTSucc :: EP
+--threeWhichLTSucc = do
+--                   x <- inj 3
+--                   comma rcLTSucc x
+
+-- indefinites
+
+domain :: S
+domain = [1..5]
+
+indef :: (E -> Bool) -> StateT List E
+indef prop = StateT $ \s -> List . concat $ map (\x -> [(x, s)]) $ filter prop domain
+
+anOddWhichLTSucc :: EP
+anOddWhichLTSucc = do
+                   x <- lift . push $ indef odd
+                   comma rcLTSucc x
+
+-- testing
+--
+-- binding into and out of NRC
+-- 3, which is less than its successor, is less than it
+check :: WriterT (StateT List) Bool
+check = return (<) <*> anOddWhichLTSucc <*> lift (pro 0)
 
 display :: WriterT (StateT List) a -> [((a, Bool), S)]
 display (WriterT (StateT f)) = runList $ f []
 
-add :: WriterT (StateT List) Int
-add = return (+) <*> inj 3 <*> inj 2
-
 main :: IO ()
-main = print . display $ add
+main = print . display $ check
