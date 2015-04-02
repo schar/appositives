e599fbd72521cd365aa7d7daf54368bf1ffc738e
Simon Charlow <simon.charlow@gmail.com>
2015-04-01 21:47:28 -0400

    newComma, better stacking

--- appos_new.hs
+++ appos_new.hs
@@ -16,13 +16,14 @@ instance Monoid Bool where
   mappend = (&&)
 
 -- anaphora: push, λ, pro
+lambda :: MonadState S m => m a -> E -> m a
+lambda m x = modify (++[x]) >> m
+
 push :: MonadState S m => m E -> m E
 push m = do
          x <- m
-         modify (++[x]) >> return x
-
-lambda :: MonadState S m => m a -> E -> m a
-lambda m x = modify (++[x]) >> m
+         modify (++[x]) >> return x -- notice that push's functionality
+                                    -- is fully recoverable from lambda
 
 pro :: MonadState S m => Int -> m E
 pro n = gets $ (!!n) . reverse
@@ -36,6 +37,9 @@ comma :: (E -> P) -> E -> EP
 comma f x = WriterT $ do
                       p <- f x
                       return (x, p) -- DB: = \f -> listen . lift . f?
+
+newComma :: MonadWriter T m => (E -> m T) -> E -> m E
+newComma f x = do { p <- f x; tell p; return x }
 
 rcLTSucc,rcGTSucc :: E -> P
 rcLTSucc x = do
@@ -51,7 +55,7 @@ rcGTSucc x = do
 domain :: S
 domain = [1..10]
 
-indef :: (E -> T) -> StateT S [] E
+indef :: (E -> T) -> ST [] E
 indef prop = mfilter prop $ lift domain
 
 anOddWhichLTSucc :: EP
@@ -96,30 +100,35 @@ text1, text2 :: TP
 text1 = return (&&) <*> check <*> lift (do { x <- pro 0; return $ odd x })
 text2 = return (&&) <*> check <*> lift (do { x <- pro 1; return $ odd x })
 
--- stacked appositives
--- [three, which l.t. its successor], which l.t. it, is odd
+-- an odd, which l.t. [its successor, which g.t. it]
+-- is odd. this is much smoother! niiiiiiiiiiiiiice.
 stacked1,stacked2 :: TP
 stacked1 = do
-  x <- lift . push . return $ 3
-  y <- comma rcLTSucc x
-  z <- comma (\u -> do {v <- pro 0; return $ (<) u v }) y
-  return $ odd z
--- three, which l.t. [its successor, which g.t. it], is odd
+  o <- lift $ indef odd
+  x <- newComma (lambda nrr1) o
+  return $ odd x
+  where nrr1 = do
+               y <- lift $ pro 0
+               z <- newComma (lambda nrr2) $ succ y
+               return $ y < z
+        nrr2 = do
+               y <- lift $ pro 0
+               z <- lift $ pro 1
+               return $ y > z
+-- [an odd, which l.t. its succ], which l.t. it, is odd
 stacked2 = do
-  x <- lift . push . return $ 3                            -- 3 [+ dref]
-  y <- lift . push $ return succ <*> pro 0                 -- 3's succ [+ dref]
-  z <- comma (\u -> do { v <- pro 1; return $ (>) u v }) y -- 3's succ, which > 3
-  u <- comma (\v -> return $ (<) v z) x                    -- 3, which < its succ
-  return $ odd u -- perhaps cleaner to separate the
-                 -- 1D-to-2D piping step from the
-                 -- toggling between the two monads..
-                 -- that way, can assume that piping
-                 -- always operates on functions into
-                 -- WriterT, avoid odd scopings like
-                 -- those in evidence in stacked2
-
-display :: WriterT T (StateT S []) a -> [((a, T), S)]
-display (WriterT (StateT f)) = f []
+  o <- lift $ indef odd
+  x <- newComma (lambda nrr1) o
+  y <- newComma (lambda nrr2) x
+  return $ odd y
+  where nrr1 = lift $ do
+                      x <- pro 0
+                      y <- push . return $ succ x
+                      return $ x < y
+        nrr2 = lift $ do
+                      x <- pro 0
+                      y <- pro 1
+                      return $ x < y
 
 -- disjunction facts: can supplement a
 -- disjunction, but cannot directly disjoin
@@ -131,14 +140,34 @@ disj :: ST [] a -> ST [] a -> ST [] a
 disj = mplus
 
 -- as noted, it *is* possible for a disjunction
--- (though not a disjunct) to be supplemented:
+-- (though not a disjunct) to be supplemented.
+-- the cases are not the smoothest, but i'd
+-- expect this has to do with disjunctions not
+-- being particularly good antecedents for
+-- discourse anaphora in the first place.
 twoOrThree :: ST [] E
-twoOrThree = push $ return 2 `disj` return 3
+twoOrThree = return 2 `disj` return 3
 
 -- two or three, which is odd
 twoOrThreeWhichOdd :: EP
-twoOrThreeWhichOdd = do { x <- lift twoOrThree; comma rel x }
-  where rel x = return $ odd x
+twoOrThreeWhichOdd = do { x <- lift twoOrThree; newComma (lambda rel) x }
+  where rel = do { x <- pro 0; return $ odd x }
+
+-- an even, which l.t. its successor, l.t. it
+bindInOut :: TP
+bindInOut = do
+  x <- lift $ indef even
+  y <- newComma (lambda nrr) x
+  z <- pro 0
+  return $ y < z
+  where nrr = lift $ do
+                     y <- pro 0
+                     u <- push . return $ succ y
+                     return $ y < u
+
+-- IO
+display :: WriterT T (StateT S []) a -> [((a, T), S)]
+display (WriterT (StateT f)) = f []
 
 main :: IO ()
 main = putStrLn "DO STHG"
